06631d41783f3b5c7981fc6ede95265fa4cfea81
Sam Schauer <samschauer@users.noreply.github.com>
2018-05-01 15:22:09 -0600

    fix: correct AltBeacon data length calculation
    fixes incompatibility with Android 7.0+

--- test-altbeacon.js
+++ test-altbeacon.js
@@ -6,7 +6,7 @@ bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertisingAltBeacon(0x0002, '0102030405060708090a0b0c0d0e0f99', 0xB9, -99);
+    bleno.startAdvertisingAltBeacon(0x0002, '0102030405060708090a0b0c0d0e0f99', 0xB9, 55);
   } else {
     bleno.stopAdvertising();
   }
